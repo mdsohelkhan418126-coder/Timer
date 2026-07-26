32abcfa768ff028804411445d62ad5f46809c4a2
sohelkhan <mdsohelkhan418126@gmail.com>
2026-07-26 22:19:32 +0600

    Add initial implementation of Breathing Timer with HTML, CSS, and JavaScript

---
+++ app.js
@@ -0,0 +1,9 @@
+const timerDisplay = document.getElementById('timer');
+const startBtn = document.getElementById('startbtn');
+const stopbtn = document.getElementById('stopbtn');
+startBtn.addEventListener('click', () => {
+  console.log('hello')
+});
+stopbtn.addEventListener('click', () => {
+  console.log('stop')
+});
