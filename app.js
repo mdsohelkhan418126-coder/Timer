f45d54a3fd5f5a184f1c19e0b6d75fb8fe5ba534
sohelkhan <mdsohelkhan418126@gmail.com>
2026-07-28 19:55:26 +0600

    Enhance breathing timer functionality and improve UI responsiveness

--- app.js
+++ app.js
@@ -1,9 +1,46 @@
 const timerDisplay = document.getElementById('timer');
 const startBtn = document.getElementById('startbtn');
 const stopbtn = document.getElementById('stopbtn');
+
+let timeLeft;
+let timeInterval;
+let isRunnig = false;
+
+const startimer = duration => {
+  let timer = duration;
+  timeInterval = setInterval(() => {
+    const minuts = parseInt(timer / 60, 10);
+    const second = parseInt(timer % 60, 10);
+    const displayMinuts = minuts < 10 ? '0' + minuts : minuts;
+    const displaySeconds = second < 10 ? '0' + second : second;
+    timerDisplay.textContent = displayMinuts + ':' + displaySeconds;
+
+    if (--timer < 0) {
+      clearInterval(timeInterval);
+      timerDisplay.textContent = 'Breath Out';
+      setTimeout(() => {
+        timerDisplay.textContent = 'Breath In';
+        startimer(timeLeft);
+      }, 3000);
+    }
+  }, 1000);
+};
+
+const stoptimer = () => {
+  clearInterval(timeInterval);
+  timerDisplay.textContent = 'Breath In';
+  isRunnig = false;
+};
+
 startBtn.addEventListener('click', () => {
-  console.log('hello')
+  if (!isRunnig) {
+    timeLeft = 120;
+    startimer(timeLeft);
+    isRunnig = true;
+  }
 });
+
 stopbtn.addEventListener('click', () => {
-  console.log('stop')
+  stoptimer();
+  isRunnig = false;
 });
